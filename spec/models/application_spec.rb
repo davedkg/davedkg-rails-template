0f39a9d62599924f4ef9a6047b08a3fd363b7f6f
davedkg <davedkg@gmail.com>
2018-07-29 23:12:28 -0400

    add application model specs

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,27 @@
-# require 'rails_helper'
-#
-# RSpec.describe Application, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  
+  let(:application_params) { attributes_for(:application) }
+  let(:application) { Application.create(application_params) }
+  
+  context "#valid?" do
+    it "requires a name" do
+      application_params.delete(:name)
+      expect(Application.new(application_params).valid?).to be(false)
+    end
+  end
+  
+  context "#create" do
+    it "generates a key" do
+      expect(application.key).not_to be_empty
+    end
+  end
+  
+  context "#save" do
+    it "downcases lname" do
+      expect(application.lname).to eq(application_params[:name].downcase)
+    end
+  end
+  
+end
