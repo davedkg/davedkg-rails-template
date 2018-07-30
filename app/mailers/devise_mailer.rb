8416b22a06a783e6a81dac5df9269e40c884f5d9
davedkg <davedkg@gmail.com>
2018-07-29 22:46:02 -0400

    change email subjects

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,17 +2,23 @@ class DeviseMailer < Devise::Mailer
   
   def confirmation_instructions(user_id, token, opts={})
     sendgrid_category "Devise - Confirmation Instructions"
-    super(User.find(user_id), token, opts)
+    mail = super(User.find(user_id), token, opts)
+    mail.subject = "[" + APP_TITLE + "] Confirm your Email"
+    mail
   end
   
   def invitation_instructions(user_id, token, opts={})
     sendgrid_category "Devise - Invitaton Instructions"
-    super(User.find(user_id), token, opts)
+    mail = super(User.find(user_id), token, opts)
+    mail.subject = "[" + APP_TITLE + "] Invitation Instructions"
+    mail
   end
 
   def reset_password_instructions(user_id, token, opts={})
     sendgrid_category "Devise - Reset Password Instructions"
-    super(User.find(user_id), token, opts)
+    mail = super(User.find(user_id), token, opts)
+    mail.subject = "[" + APP_TITLE + "] Change your Password"
+    mail
   end
   
 end
