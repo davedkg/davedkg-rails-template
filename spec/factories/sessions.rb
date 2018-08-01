97100869423c2c207982b18d8fbffb8a476a1e8b
davedkg <davedkg@gmail.com>
2018-08-01 00:32:51 -0400

    add api sessions endpoint specs

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -3,4 +3,9 @@ FactoryBot.define do
     user { create(:user) }
     ip_address Faker::Internet.private_ip_v4_address
   end
+  
+  factory :device_session, class: Sessions::DeviceSession do
+    user { create(:user) }
+    ip_address Faker::Internet.private_ip_v4_address
+  end
 end
