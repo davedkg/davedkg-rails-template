276c1de49cc278b2fe6573f697c72ac57a687b0f
davedkg <davedkg@gmail.com>
2018-12-02 03:16:05 -0500

    upgrade to rails 5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,13 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -30,10 +31,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
-  config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN'] }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -49,9 +46,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
+
+  config.action_mailer.default_url_options = { host: ENV['APP_DOMAIN'] }
   config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 end
