ea0196fb7a14944400d6c11395b6371456f1a376
davedkg <davedkg@gmail.com>
2020-04-07 06:41:07 -0400

    Add heroku rows count widget to dashboard

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,4 +5,14 @@ class ApplicationRecord < ActiveRecord::Base
 
   self.abstract_class = true
 
+  def self.rows_count
+    c = 0
+
+    ApplicationRecord.descendants.each do |model|
+      c += model.unscoped.count
+    end
+
+    c
+  end
+
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,2 @@
-PLATFORM_TITLE = "DKG Template"
+PLATFORM_TITLE     = "DKG Template"
+HEROKU_ROWS_COUNT = 10000 # number of rows available on herokus postgres db
