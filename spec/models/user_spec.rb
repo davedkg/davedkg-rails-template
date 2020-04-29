c20a1cb2a1ede5646f4c7d2a713f32038486c804
davedkg <davedkg@gmail.com>
2020-04-29 14:33:09 -0400

    add subject support to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,50 +8,42 @@ describe User do
   let(:user_attributes) { attributes_for(:user) }
 
   describe "#valid?" do
+    subject { User.new(user_attributes) }
+
     it "returns false when email is missing" do
       user_attributes.delete(:email)
 
-      expect(User.new(user_attributes).valid?).to be(false)
+      expect(subject).not_to be_valid
     end
 
     it "requires avatar of content_type image" do
       user_attributes[:avatar] = FilesSpecHelper.txt
 
-      expect(User.new(user_attributes)).not_to be_valid
+      expect(subject).not_to be_valid
+    end
+
+    it "requires a unique email address" do
+      User.create(user_attributes)
+
+      expect(subject).not_to be_valid
     end
 
     it "requires avatar with width and height equal to 200px" do
       user_attributes[:avatar] = FilesSpecHelper.png_150x150
 
-      expect(User.new(user_attributes)).not_to be_valid
+      expect(subject).not_to be_valid
     end
 
     context "when updating user" do
-      let!(:user) { create(:user) }
+      subject { create(:user) }
 
-      it "returns false when name is missing" do
-        user_attributes[:name] = nil
+      it "returns false when name is blank" do
+        user_attributes[:name] = ""
+        subject.attributes     = user_attributes
 
-        user.update(user_attributes)
-
-        expect(user.valid?).to be(false)
+        expect(subject).not_to be_valid
       end
     end
   end
-
-  describe "#create" do
-    it "DOES NOT create two users with the same email address" do
-      User.create(user_attributes)
-      User.create(user_attributes)
-
-      expect(User.count).to eq(1)
-    end
-  end
-
-  # FIXME
-  # describe "#send_invitation" do
-  #   it "sends an invitationm email"
-  #   it "sets invitation_sent_at"
-  # end
 
 end
