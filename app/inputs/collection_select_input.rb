1f1c9fe5f55731b291ac3512cd9bd309c91e4cb3
davedkg <davedkg@gmail.com>
2020-04-29 02:03:33 -0400

    skin select input

--- app/inputs/collection_select_input.rb
+++ app/inputs/collection_select_input.rb
@@ -1,3 +1,13 @@
 class CollectionSelectInput < SimpleForm::Inputs::CollectionSelectInput
   include Inputs::Stimulusable
+
+  def input_html_options
+    options = super
+
+    options[:data] ||= {}
+    options[:data][:placeholder] = (options[:placeholder] || "choose an option...")
+
+    options
+  end
+
 end
