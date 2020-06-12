bf163d6c9d92ccb26b0ff490f5f9fe4350a5ecf3
davedkg <davedkg@gmail.com>
2020-06-12 18:09:59 -0400

    change mailgun config var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_PUBLIC_KEY'],
+    api_key: ENV['MAILGUN_API_KEY'],
     domain: ENV['MAILGUN_DOMAIN'],
     # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   }
