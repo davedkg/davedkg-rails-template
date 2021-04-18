dbd8ef09b62e081d88e0aa0a18c5c8b38edee15d
davedkg <davedkg@gmail.com>
2021-04-18 04:42:11 -0400

    Use sweetalert2 for data-confirm

---
+++ app/javascript/lib/confirm.js
@@ -0,0 +1,27 @@
+import Swal from 'sweetalert2'
+
+Rails.confirm = function(message, element) {
+  let $element = $(element)
+
+  Swal.fire({
+    title: message,
+    icon: 'warning',
+    showCancelButton: true,
+    showDenyButton: false,
+    confirmButtonText: 'Continue',
+    buttonsStyling: false,
+    confirmButtonClass: 'btn btn-primary ml-3',
+    cancelButtonClass: 'btn btn-link mr-3',
+    reverseButtons: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let old = Rails.confirm
+
+      Rails.confirm = function() { return true }
+      $element.get(0).click()
+      Rails.confirm = old
+    }
+  })
+
+  return false;
+}
