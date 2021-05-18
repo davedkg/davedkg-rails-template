fa6ded5be60da1b2bfccb5dd866545adfa4d08f1
davedkg <davedkg@gmail.com>
2021-05-18 15:18:17 -0400

    [ci skip] init drop of native user agent support

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,3 +2,7 @@
 
 PLATFORM_TITLE = 'DKG Template'
 HEROKU_ROWS_COUNT = 10_000 # number of rows available on herokus postgres db
+
+# Native User Agents
+NATIVE_IOS_USER_AGENT = "TemplateiOS"
+NATIVE_ANDROID_USER_AGENT = "TemplateAndroid"
