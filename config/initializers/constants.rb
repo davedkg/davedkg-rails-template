a056d65d4ad3a788b56d327b4f6462a4a5fde475
davedkg <davedkg@gmail.com>
2022-05-15 01:18:27 -0400

    add ENV['PLATFORM_TITLE'] support

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@
 
 require_relative Rails.root.join('lib', 'app_config.rb')
 
-PLATFORM_TITLE = 'DKG Template'
+PLATFORM_TITLE = ENV['PLATFORM_TITLE'] || 'DKG Template'
 HEROKU_ROWS_COUNT = 10_000 # number of rows available on herokus postgres db
 APP_DOMAIN = AppConfig.app_domain
 EMAIL_DOMAIN = AppConfig.email_domain
