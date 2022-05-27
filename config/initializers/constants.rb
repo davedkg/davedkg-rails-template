fea8b9beff7d602933bfb5947be5c7a85b28730b
davedkg <davedkg@gmail.com>
2022-05-27 12:44:22 -0400

    add env.HEROKU_ROWS_COUNT support

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,7 +3,7 @@
 require_relative Rails.root.join('lib', 'app_config.rb')
 
 PLATFORM_TITLE = ENV['PLATFORM_TITLE'] || 'DKG Template'
-HEROKU_ROWS_COUNT = 10_000 # number of rows available on herokus postgres db
+HEROKU_ROWS_COUNT = ENV['HEROKU_ROWS_COUNT'] ||  10_000 # number of rows available on herokus postgres db
 APP_DOMAIN = AppConfig.app_domain
 EMAIL_DOMAIN = AppConfig.email_domain
 
