98fec93dc3f3df2f62c6aa7fef7a4ea024714c88
davedkg <davedkg@gmail.com>
2024-04-09 15:33:08 -0400

    add active sidebar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def active_sidebar_link_for_path(path)
+    if request.path.start_with?(path)
+      'sidebar-link active'
+    else
+      'sidebar-link'
+    end
+  end
 end
