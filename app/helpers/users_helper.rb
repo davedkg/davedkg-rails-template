b625c5ec527226b363fbbd045a0e1d20ebec69a0
davedkg <davedkg@gmail.com>
2024-04-11 17:06:10 -0400

    paginate users using turbo stream

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,13 @@
 module UsersHelper
   def user_status_badge(user)
     if !user.accepted_invitation?
-      content_tag(:span, 'invited', class: %w[badge badge-secondary])
+      content_tag(:span, 'invited', class: %w[badge text-bg-secondary])
     elsif user.locked?
-      content_tag(:span, 'locked', class: %w[badge badge-danger])
+      content_tag(:span, 'locked', class: %w[badge text-bg-danger])
     elsif user.disabled?
-      content_tag(:span, 'disabled', class: %w[badge badge-danger])
+      content_tag(:span, 'disabled', class: %w[badge text-bg-danger])
     else
-      content_tag(:span, 'active', class: %w[badge badge-success])
+      content_tag(:span, 'active', class: %w[badge text-bg-success])
     end
   end
 
