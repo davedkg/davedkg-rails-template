d953d70a1ea371a00b300ef8559f93209e534ed5
davedkg <davedkg@gmail.com>
2024-04-11 18:32:19 -0400

    remove sidebar nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 import * as bootstrap from "bootstrap";
 
 import "@hotwired/turbo-rails";
-import "./modernize/sidebarmenu";
 import "./modernize/sidebar";
 import "./lib/confirm";
 import "./lib/toast";
