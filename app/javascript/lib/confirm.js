db0635504efd9b22b754d49385cfe2c2121674e6
davedkg <davedkg@gmail.com>
2024-04-13 19:26:24 -0400

    stylize sweetalert2

--- app/javascript/lib/confirm.js
+++ app/javascript/lib/confirm.js
@@ -4,14 +4,7 @@ Turbo.setConfirmMethod((message, _element) => {
   return new Promise((resolve, reject) => {
     Swal.fire({
       title: message,
-      icon: 'warning',
       showCancelButton: true,
-      showDenyButton: false,
-      confirmButtonText: 'Continue',
-      buttonsStyling: false,
-      confirmButtonClass: 'btn btn-primary ml-3',
-      cancelButtonClass: 'btn btn-default mr-3',
-      reverseButtons: true,
     }).then((result) => {
       resolve(result.isConfirmed)
     }).catch(error => { reject(error) })
