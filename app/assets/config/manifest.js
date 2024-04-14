539de7511d5b2a4ee868d4e4de068228adab4993
davedkg <davedkg@gmail.com>
2024-04-13 23:14:12 -0400

    cleanup asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
-//= link_tree ../../../vendor/assets
+//= link_tree ../../../node_modules/@fortawesome/fontawesome-free/webfonts
+//= link_tree ../../../node_modules/tabler-icons/iconfont/fonts
 //= link_tree ../images
 //= link_tree ../builds
 //= link application.css
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 import * as bootstrap from "bootstrap";
 
 import "@hotwired/turbo-rails";
-import "./modernize/sidebar";
+import "../../vendor/javascript/modernize/sidebar";
 import "./lib/confirm";
-import "./lib/toast";
 import "./controllers";
