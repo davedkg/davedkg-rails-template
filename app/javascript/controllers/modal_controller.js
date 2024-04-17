050d49424767e7a1708522319309f2bdfdc9d734
davedkg <davedkg@gmail.com>
2024-04-17 17:05:02 -0400

    remove model when closed

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,5 +4,9 @@ import { Modal } from "bootstrap";
 export default class extends Controller {
   connect() {
     new Modal(this.element).show();
+
+    this.element.addEventListener("hidden.bs.modal", () => {
+      this.element.remove();
+    });
   }
 }
