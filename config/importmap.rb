b7618e92f00435a26d4d4839734aae321b863136
davedkg <davedkg@gmail.com>
2024-05-09 23:54:54 -0400

    use local version of popper

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "nifty", to: "nifty.js"
-pin "bootstrap" # @5.3.3
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.8/lib/index.js"
+pin "bootstrap"
+pin "@popperjs/core", to: "popper.js"
