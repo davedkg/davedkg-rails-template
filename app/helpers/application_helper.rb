f3d25a45ddf72b06bb4bf0946b044d8e7c24b554
davedkg <davedkg@gmail.com>
2024-05-15 22:43:07 -0400

    update sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def active_sidebar_tab_for_path(path)
+    "nav-link mininav-toggle #{request.path.start_with?(path) ? 'active' : nil}"
+  end
 end
