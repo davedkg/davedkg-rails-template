560a495d8e7d85152571d2a6368060319f0b38f9
davedkg <davedkg@gmail.com>
2024-05-18 00:05:00 -0400

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   resources :web_components, only: [:index], path: :"web-components"
 
-  root 'dashboard#show'
+  root 'application#root'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
