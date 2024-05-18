53e20c9c2fa077d1af42054e9a6a30b9b3f5ea36
davedkg <davedkg@gmail.com>
2024-05-17 21:25:40 -0400

    fasterer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@
 
 require 'sidekiq/web'
 
-database_url = ENV.fetch('REDIS_URL', nil)
+database_url = ENV.fetch('REDIS_URL') { 'redis://127.0.0.1:6379/0' }
 
 Sidekiq.configure_server do |config|
   config.redis = { url: database_url }
