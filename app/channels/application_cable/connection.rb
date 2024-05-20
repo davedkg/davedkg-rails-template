d12a047178e7ccad08f60cdb3f114093223f48d9
davedkg <davedkg@gmail.com>
2024-05-19 21:11:39 -0400

    add ActionCable testing

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,20 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verfied_user
+    end
+
+    protected
+
+    def find_verfied_user
+      if (current_user = env['warden'].user)
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
