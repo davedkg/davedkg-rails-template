490575eeedf0c06d230ffbb96e89a69941281824
davedkg <davedkg@gmail.com>
2024-05-19 21:13:06 -0400

    Add ActiveJob testing

---
+++ spec/jobs/application_job_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ApplicationJob do
+  describe '#perform_later' do
+    it 'queues a job' do
+      expect { described_class.perform_later('dummy_job') }.to have_enqueued_job
+    end
+  end
+end
