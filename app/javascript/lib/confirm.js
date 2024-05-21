b23240f5d005d6206e14473ff7856bbe51dec3f3
davedkg <davedkg@gmail.com>
2024-05-21 15:32:39 -0400

    fix confirm loading

--- app/javascript/lib/confirm.js
+++ app/javascript/lib/confirm.js
@@ -1,12 +1,16 @@
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 Turbo.setConfirmMethod((message, _element) => {
   return new Promise((resolve, reject) => {
     Swal.fire({
       title: message,
       showCancelButton: true,
-    }).then((result) => {
-      resolve(result.isConfirmed)
-    }).catch(error => { reject(error) })
-  })
-})
+    })
+      .then((result) => {
+        resolve(result.isConfirmed);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+});
