107e6143ea88f18f78b8baaf7e8e66f829f955bd
davedkg <davedkg@gmail.com>
2024-05-23 19:18:38 -0400

    fix css and most js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../vendor/assets/stylesheets
 //= link_tree ../../../vendor/assets/images
 //= link_tree ../../../vendor/assets/fonts
+//= link_tree ../builds
