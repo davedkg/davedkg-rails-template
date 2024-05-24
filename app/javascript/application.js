2a1074c4d91df13874cb8afb1aa0cb2c7268dcd5
David Guilfoyle <davedkg@users.noreply.github.com>
2024-05-23 20:03:54 -0400

    Migrate from Importmaps to ESBuild (#100)
    * remove vendor bootstrap
    
    * fix css and most js
    
    * move stylesheets around
    
    * remove bootstrap gem
    
    * update stimulus controllers
    
    * clean up table rows
    
    * fix rubocop complaint about named subject
    
    * fix css lint issues
    
    * add assets:precompile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 import "@hotwired/turbo-rails";
-import "@popperjs/core";
-import * as bootstrap from "bootstrap";
-import "nifty";
-import "lib/confirm";
-import "controllers";
+import "bootstrap";
+import "../../vendor/javascript/nifty";
+import "./lib/confirm";
+import "./controllers";
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/lib", under: "lib"
-pin "nifty", to: "nifty.js"
-pin "bootstrap"
-pin "@popperjs/core", to: "popper.js"
-pin "sweetalert2" # @11.11.0
-pin "autosize" # @6.0.1
+
