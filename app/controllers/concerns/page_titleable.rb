aaf1c5ab7cfdc4de669056cf0f107b6cef88a8b1
David Guilfoyle <davedkg@users.noreply.github.com>
2024-12-15 04:37:40 -0500

    add PageTitleable

---
+++ app/controllers/concerns/page_titleable.rb
@@ -0,0 +1,37 @@
+module PageTitleable
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :page_title, :page_subtitle
+  end
+
+  private
+
+  def page_title_resource
+    controller_name.titleize.singularize
+  end
+
+  def page_title_hash
+    {
+      index: page_title_resource.pluralize.to_s,
+      show: page_title_resource.to_s,
+      new: "New #{page_title_resource}",
+      edit: "Edit #{page_title_resource}",
+      create: "New #{page_title_resource}",
+      update: "Edit #{page_title_resource}",
+      destroy: "Destroy #{page_title_resource}"
+    }
+  end
+
+  def page_subtitle_hash
+    {}
+  end
+
+  def page_title
+    @page_title ||= page_title_hash[params[:action].to_sym]
+  end
+
+  def page_subtitle
+    @page_subtitle ||= page_subtitle_hash[params[:action].to_sym]
+  end
+end
