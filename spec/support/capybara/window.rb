73e1eb7be9872f1869c4d1c52082844afe0b6f26
David Guilfoyle <davedkg@users.noreply.github.com>
2024-12-16 00:58:32 -0500

    Add Dashboard feature tests

---
+++ spec/support/capybara/window.rb
@@ -0,0 +1,23 @@
+module Capybara
+  class Window
+    CAPYBARA_WINDOW_SIZES = %i[xs sm md lg xl].freeze
+
+    def resize_to_preset(preset)
+      dimensions = case preset
+                   when :xs, :extra_small, :sm, :small
+                     [576, 627]
+                   when :md, :medium
+                     [768, 640]
+                   when :lg, :large
+                     [992, 768]
+                   when :xl, :extra_large
+                     [1200, 800]
+                   else
+                     raise ArgumentError, "#{preset} is not a valid preset"
+                   end
+      resize_to(*dimensions)
+    end
+  end
+end
+
+Selenium::WebDriver.logger.ignore(:clear_local_storage, :clear_session_storage)
