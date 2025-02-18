dab6c3d130dfa57c736ed7ddfc141d79c1260976
David Guilfoyle <davedkg@users.noreply.github.com>
2025-02-18 13:45:52 -0500

    open/close sidebar on lg+

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ "sidebar" ]
+  static targets = [ "sidebar", "main" ]
 
   show() {
+    this.mainTarget.classList.add("sidebar-shown")
+    this.mainTarget.classList.remove("sidebar-hidden")
     this.sidebarTarget.classList.add("show")
     this.sidebarTarget.classList.remove("hide")
   }
 
   hide() {
+    this.mainTarget.classList.remove("sidebar-shown")
+    this.mainTarget.classList.add("sidebar-hidden")
     this.sidebarTarget.classList.add("hide")
     this.sidebarTarget.classList.remove("show")
   }
