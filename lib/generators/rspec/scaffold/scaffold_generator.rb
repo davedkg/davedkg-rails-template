c84fd0e1f1d08c526babc2366e95357a7f1682ea
David Guilfoyle <davedkg@users.noreply.github.com>
2025-02-21 23:07:31 -0500

    another ci hack

--- lib/generators/rspec/scaffold/scaffold_generator.rb
+++ lib/generators/rspec/scaffold/scaffold_generator.rb
@@ -1,9 +1,10 @@
 require "generators/rspec"
-require "rails/generators/resource_helpers"
 
 if Rails.env.test?
   class Rspec::Generators::ScaffoldGenerator; end
 else
+  require "rails/generators/resource_helpers"
+
   module Rspec
     module Generators
       # @private
