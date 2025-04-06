5e893cb2ed2a2d35fa48e44e6ac1825e6885c9a2
David Guilfoyle <davedkg@users.noreply.github.com>
2025-04-06 17:22:44 -0400

    Enable LetterOpener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
 
   # Devise config
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
