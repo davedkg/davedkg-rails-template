f7821a841630071201f2f62743e246717b666227
David Guilfoyle <davedkg@users.noreply.github.com>
2026-02-04 13:17:08 -0500

    rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,17 +58,17 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Highlight code that triggered redirect in logs.
-  config.action_dispatch.verbose_redirect_logs = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
+
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
